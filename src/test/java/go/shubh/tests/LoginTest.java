98eb468acd23d67f0830e52771a5488a773f947f
go-shubh <go.shubh@outlook.com>
2024-05-29 01:03:50 +0530

    added print for title and heading

--- src/test/java/go/shubh/tests/LoginTest.java
+++ src/test/java/go/shubh/tests/LoginTest.java
@@ -13,7 +13,8 @@ public class LoginTest extends BaseTest {
         LoginPage loginPage = new LoginPage(driver);
         loginPage.login(propReader.getProperty("username"), propReader.getProperty("password"));
 
-        ProductsPage dashboardPage = new ProductsPage(driver);
-        Assert.assertTrue(dashboardPage.getPageTitle().contains("Swag Labs") && dashboardPage.getPageHeading().contains("Products"), "Login failed or Products page not loaded");
+        ProductsPage dp = new ProductsPage(driver);
+        System.out.println(dp.getPageTitle() + dp.getPageHeading());
+        Assert.assertTrue(dp.getPageTitle().contains("Swag Labs") && dp.getPageHeading().contains("Products"), "Login failed or Products page not loaded");
     }
 }
